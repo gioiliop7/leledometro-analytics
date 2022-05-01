bfa4a3ae2b307ec36714ed60d7f0880395f960b1
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2022-05-01 09:48:33 +0300

    Not let, only consts when i should i and function to format date

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+  function formatDateString(day, month, year) {
+    month = parseInt(month) + 1; // We add one because the value of xml starts of 0. For example January is in zero position.
+    const d = new Date(`${year}-${month}-${day}`);
+    console.log(d);
+    const years = d.getFullYear();
+    const date = d.getDate();
+    const months = d.getMonth() + 1;
+    console.log(months);
+    const formattedDate = `${date}/${months}/${years}`;
+    return formattedDate;
+  }
   // Handler for .ready() called.
   $("#xmlupload").change(function () {
     var file = document.getElementById("xmlupload").files[0];
@@ -11,32 +22,32 @@ $(document).ready(function () {
       //   console.log(xmlData);
       xmlData = xmlData[1];
       //   console.log(xmlData);
-      let ypiresies = xmlData.getElementsByTagName("ipiresia");
-      let details = xmlData.getElementsByTagName("stoixeia");
-      let adeies = xmlData.getElementsByTagName("adeia");
-      let poreia = xmlData.getElementsByTagName("poreia");
+      const ypiresies = xmlData.getElementsByTagName("ipiresia");
+      const details = xmlData.getElementsByTagName("stoixeia");
+      const adeies = xmlData.getElementsByTagName("adeia");
+      const poreia = xmlData.getElementsByTagName("poreia");
       let simioseis = xmlData.getElementsByTagName("simioseis");
       let personDetails = Array.from(details);
       personDetails = personDetails[0];
-      let arrayServices = Array.from(ypiresies);
-      let arrayAdeies = Array.from(adeies);
-      let poreies = Array.from(poreia);
+      const arrayServices = Array.from(ypiresies);
+      const arrayAdeies = Array.from(adeies);
+      const poreies = Array.from(poreia);
       simioseis = simioseis[0].childNodes;
-      let asm = simioseis[5].innerText;
-      let personName = personDetails.childNodes[0];
-      let startYear = personDetails.childNodes[1];
-      let startMonth = personDetails.childNodes[2];
-      let startDay = personDetails.childNodes[3];
-      let endYear = personDetails.childNodes[4];
-      let endMonth = personDetails.childNodes[5];
-      let endDay = personDetails.childNodes[6];
-      let soma = personDetails.childNodes[7];
+      const asm = simioseis[5].innerText;
+      const personName = personDetails.childNodes[0];
+      const startYear = personDetails.childNodes[1];
+      const startMonth = personDetails.childNodes[2];
+      const startDay = personDetails.childNodes[3];
+      const endYear = personDetails.childNodes[4];
+      const endMonth = personDetails.childNodes[5];
+      const endDay = personDetails.childNodes[6];
+      const soma = personDetails.childNodes[7];
       // ARMY = 0; NAVY = 1; HAF = 2;
-      let oplo_stratou = personDetails.childNodes[8]; // It returns value only on ξηρά..
-      let seira = personDetails.childNodes[9];
-      let esso = personDetails.childNodes[10];
-      let prison = personDetails.childNodes[11];
-      let notLetMeOut = personDetails.childNodes[12];
+      const oplo_stratou = personDetails.childNodes[8]; // It returns value only on ξηρά..
+      const seira = personDetails.childNodes[9];
+      const esso = personDetails.childNodes[10];
+      const prison = personDetails.childNodes[11];
+      const notLetMeOut = personDetails.childNodes[12];
       arrayServices.forEach((element) => {
         let yp_name = element.childNodes[0].innerText;
         let yp_year = element.childNodes[1].innerText;
@@ -46,7 +57,6 @@ $(document).ready(function () {
       });
       arrayAdeies.forEach((element) => {
         let adeiaName = element.childNodes[0].innerText;
-        console.log(adeiaName);
         let adeiaYear = element.childNodes[1].innerText;
         let adeiaMonth = element.childNodes[2].innerText;
         let adeiaDay = element.childNodes[3].innerText;
