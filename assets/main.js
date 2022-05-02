18450345cf1c1fccfaf8beb206f5a023bfc1c98c
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2022-05-02 20:05:47 +0300

    Photos,start of html and styling

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+  let location = window.location.href;
+  let directoryPath = location.substring(0, location.lastIndexOf("/") + 1);
+
+  function percentage(partialValue, totalValue) {
+    return (100 * partialValue) / totalValue;
+  }
+
   function formatDateString(day, month, year) {
     month = parseInt(month) + 1; // We add one because the value of xml starts of 0. For example January is in zero position.
     const d = new Date(`${year}-${month}-${day}`);
@@ -7,6 +14,29 @@ $(document).ready(function () {
     const months = d.getMonth() + 1;
     const formattedDate = `${date}/${months}/${years}`;
     return formattedDate;
+  }
+
+  function fadeOutElement(el) {
+    el.style.opacity = 1;
+    (function fade() {
+      if ((el.style.opacity -= 0.1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+
+  function fadeInElement(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += 0.1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
   }
 
   function maxElement(array) {
@@ -55,7 +85,22 @@ $(document).ready(function () {
 
   $("#xmlupload").change(function () {
     var file = document.getElementById("xmlupload").files[0];
-    //You could insert a check here to ensure proper file type
+    if (file.type != "text/xml") {
+      alert("Please select JSON files only!");
+      this.value = "";
+      return;
+    }
+
+    const haf_logo = directoryPath + "assets/images/air.png";
+    const army_logo = directoryPath + "assets/images/seal.png";
+    const navy_logo = directoryPath + "assets/images/navy.png";
+
+    const dashboard = document.getElementById("dashboard");
+    const uploadform = document.getElementById("uploadform");
+
+    fadeOutElement(uploadform);
+    fadeInElement(dashboard);
+
     var reader = new FileReader();
     reader.readAsText(file);
     reader.onloadend = function () {
@@ -75,7 +120,13 @@ $(document).ready(function () {
       const arrayAdeies = Array.from(adeies);
       const poreies = Array.from(poreia);
       simioseis = simioseis[0].childNodes;
-      const asm = simioseis[5].innerText;
+      let asm = simioseis[5].innerText;
+      if (asm.length == 0 || !asm.includes("/")) {
+        asm = "Δεν υπάρχει αποθηκευμένος ορθός στρατιωτικός αριθμός";
+      }
+
+      const countYp = arrayServices.length;
+      const countAdeies = arrayAdeies.length;
 
       const personName = personDetails.childNodes[0].innerText;
       const startYear = personDetails.childNodes[1].innerText;
@@ -84,20 +135,52 @@ $(document).ready(function () {
       const endYear = personDetails.childNodes[4].innerText;
       const endMonth = personDetails.childNodes[5].innerText;
       const endDay = personDetails.childNodes[6].innerText;
-      const soma = personDetails.childNodes[7].innerText;
+      let soma = personDetails.childNodes[7].innerText;
+
+      let logo;
+      let somaColor;
       // ARMY = 0; NAVY = 1; HAF = 2;
-      const oplo_stratou = personDetails.childNodes[8]; // It returns value only on ξηρά..
-      const seira = personDetails.childNodes[9];
-      const esso = personDetails.childNodes[10];
-      const prison = personDetails.childNodes[11];
-      const notLetMeOut = personDetails.childNodes[12];
+      switch (soma) {
+        case "0":
+          soma = "Στρατός Ξηράς";
+          logo = army_logo;
+          somaColor = '#fde81a';
+          break;
+        case "1":
+          soma = "Πολεμικό Ναυτικό";
+          logo = navy_logo;
+          somaColor = '#1b4279';
+          break;
+        case "2":
+          soma = "Πολεμική Αεροπορία";
+          logo = haf_logo;
+          somaColor = '#008bd0';
+          break;
+        default:
+          break;
+      }
+
+      const oplo_stratou = personDetails.childNodes[8].innerText; // It returns value only on ξηρά..
+      const seira = personDetails.childNodes[9].innerText;
+      const esso = personDetails.childNodes[10].innerText;
+      const prison = personDetails.childNodes[11].innerText;
+      const notLetMeOut = personDetails.childNodes[12].innerText;
 
       const startDate = formatDateString(startDay, startMonth, startYear);
       const endDate = formatDateString(endDay, endMonth, endYear);
       const todayDateString = todayDate();
 
-      const apolele = findDifferenceOfDays(endDate, todayDateString); // How many days for leaving army and be again citizen
+      const apolele = findDifferenceOfDays(endDate, todayDateString); // How many days for leaving army and be citizen again.
       const daysInArmy = findDifferenceOfDays(todayDateString, startDate); // How many days in army
+
+      const thiteia = findDifferenceOfDays(endDate, startDate);
+      let p = percentage(daysInArmy, thiteia);
+      p = p.toFixed(2);
+
+      const htmlPercentage = `<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${p}" aria-valuemin="0" aria-valuemax="100" style="width:${p}%"></div>`;
+      $("#pr").append(htmlPercentage);
+      document.getElementById('the-percentage').innerText = ` ${p}%`;
+      document.querySelector('.progress-bar').style.backgroundColor = somaColor;
 
       let yp_names = [];
       let adeies_names = [];
@@ -128,6 +211,40 @@ $(document).ready(function () {
 
       const mfYpiresia = maxElement(yp_names);
       const mfAdeia = maxElement(adeies_names);
+
+      document.getElementById("person-name").innerText = personName;
+      document.getElementById(
+        "esso-seira"
+      ).innerText = `${esso} ΕΣΣΟ/${startYear} - Σειρά:${seira}`;
+      document.getElementById("asm").innerText = asm;
+      document.getElementById("soma").innerText = soma;
+      document.getElementById("somaimg").src = logo;
+      document.getElementById("done").setAttribute("num", daysInArmy);
+      document.getElementById("todo").setAttribute("num", apolele);
+      document.getElementById("services").setAttribute("num", countYp);
+      document.getElementById("adeies").setAttribute("num", countAdeies);
+      document.getElementById("sex").setAttribute("num", notLetMeOut);
+      document.getElementById("jail").setAttribute("num", prison);
+
+      const counters = document.querySelectorAll(".box-number");
+      const speed = 800;
+
+      counters.forEach((counter) => {
+        const animate = () => {
+          const value = +counter.getAttribute("num");
+          const data = +counter.innerText;
+
+          const time = value / speed;
+          if (data < value) {
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 1);
+          } else {
+            counter.innerText = value;
+          }
+        };
+
+        animate();
+      });
     };
   });
 });
